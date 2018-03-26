76f7557f7f2418f4e4e45b497525ff390789830a
odooerpdevelopers <odooerpdevelopers@gmail.com>
2018-03-26 23:45:47 +0200

    IMP script odoo 11.0 install

--- odoo11-install/odoo11.sh
+++ odoo11-install/odoo11.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+ODOOVERSION=11.0
+DEPTH=10
+PATHBASE=~/Developments/odoo11
+PATHREPOS=~/Developments/odoo11/extra-addons
+
 wk32="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.1/wkhtmltox-0.12.1_linux-trusty-i386.deb"
 wk64="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb"
 
@@ -10,13 +15,19 @@ sudo apt-get install postgresql -y
 sudo su - postgres -c "createuser -s $USER"
 
 mkdir ~/Developments
-cd ~/Developments
+mkdir $PATHBASE
+mkdir $PATHREPOS
+cd $PATHBASE
 # Download Odoo from git source
-git clone https://github.com/odoo/odoo.git -b 11.0 --depth 10
+git clone https://github.com/odoo/odoo.git -b $ODOOVERSION --depth $DEPTH
 
 # Install python3 and dependencies for Odoo
+sudo apt-get install gcc python3-dev libxml2-dev libxslt1-dev \
+ libevent-dev libsasl2-dev libldap2-dev libpq-dev \
+ libpng-dev libjpeg-dev
+
 sudo apt-get -y install python3 python3-pip python-pip
-sudo pip3 install vobject qrcode num2words
+sudo pip3 install vobject qrcode num2words setuptools
 
 # Install nodejs and less
 sudo apt-get install -y npm
@@ -37,7 +48,7 @@ sudo dpkg --force-depends -i wkhtmltox*.deb
 sudo ln -s /usr/local/bin/wkhtml* /usr/bin
 
 # install python requirements file (Odoo)
-sed -i '/pyldap/d' ~/Developments/odoo/requirements.txt
-sudo pip3 install -r ~/Developments/odoo/requirements.txt
+# sed -i '/pyldap/d' $PATHBASE/odoo/requirements.txt
+sudo pip3 install -r $PATHBASE/odoo/requirements.txt
 
 
